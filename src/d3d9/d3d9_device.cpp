697f5a2f01719a7d952062f8b27192c7bfd27de0
琴梨梨OvO <24567775+qinlili23333@users.noreply.github.com>
2025-01-17 13:03:14 +1000

    Revert "[d3d9] Change correctness factor to 0.5f"
    This reverts commit 7f1e52703d921e957057e21a02079619f05d8f67.

--- src/d3d9/d3d9_device.cpp
+++ src/d3d9/d3d9_device.cpp
@@ -6689,7 +6689,12 @@ namespace dxvk {
     const D3DVIEWPORT9& vp = m_state.viewport;
 
     // Correctness Factor for 1/2 texel offset
-    constexpr float cf = 0.5f;
+    // We need to bias this slightly to make
+    // imprecision in games happy.
+    // Originally we did this only for powers of two
+    // resolutions but since NEAREST filtering fixed to
+    // truncate, we need to do this all the time now.
+    constexpr float cf = 0.5f - (1.0f / 128.0f);
 
     // How much to bias MinZ by to avoid a depth
     // degenerate viewport.
